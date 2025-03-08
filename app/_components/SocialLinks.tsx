21f1ee5477745abc64d97e41300a3d07ddab352f
Dinesh Jayasiri <ywdinesh95@gmail.com>
2025-03-08 10:07:04 +0530

    change contact

--- app/_components/SocialLinks.tsx
+++ app/_components/SocialLinks.tsx
@@ -1,12 +1,12 @@
 import { Flex, Tooltip } from '@radix-ui/themes';
-import { AtSign, FileUserIcon, GithubIcon, LinkedinIcon } from 'lucide-react';
+import { AtSign, FileUserIcon, GithubIcon, LinkedinIcon, Phone } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
 function SocialLinks() {
     return (
       <Flex gap="4">
-        <Link
+        {/* <Link
           href="https://github.com/dinesh-jaysiri"
           target="_blank"
           className="opacity-60 "
@@ -14,8 +14,8 @@ function SocialLinks() {
           <Tooltip content="Github">
             <GithubIcon strokeWidth={1} size={"20"} />
           </Tooltip>
-        </Link>
-        <Link
+        </Link> */}
+        {/* <Link
           href="https://www.linkedin.com/in/dinesh-jayasiri/"
           className="opacity-60 "
           target="_blank"
@@ -23,13 +23,22 @@ function SocialLinks() {
           <Tooltip content="Linkedin">
             <LinkedinIcon strokeWidth={1} size={"20"} />
           </Tooltip>
-        </Link>
-        <Link href="mailto:ywdinesh@gmail.com" className="opacity-60 " target="_blank">
-          <Tooltip content="ywdinesh@gmail.com">
+        </Link> */}
+        <Link
+          href="mailto:samaranayaka658@gmail.com"
+          className="opacity-60 "
+          target="_blank"
+        >
+          <Tooltip content="samaranayaka658@gmail.com">
             <AtSign strokeWidth={1} size={"20"} />
           </Tooltip>
         </Link>
-        <Link
+        <Link href="tel:+94719138538" className="opacity-60 " target="_blank">
+          <Tooltip content="tel:+94719138538">
+            <Phone strokeWidth={1} size={"20"} />
+          </Tooltip>
+        </Link>
+        {/* <Link
           href="/DineshCV2025.pdf"
           target="_blank"
           download
@@ -38,7 +47,7 @@ function SocialLinks() {
           <Tooltip content="Download resume">
             <FileUserIcon strokeWidth={1} size={"20"} />
           </Tooltip>
-        </Link>
+        </Link> */}
       </Flex>
     );
 }
